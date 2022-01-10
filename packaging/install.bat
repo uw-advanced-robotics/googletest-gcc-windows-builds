2216050e1dd81114340175478eb3330f9bec1895
MatthewMArnold <43590293+MatthewMArnold@users.noreply.github.com>
2022-01-09 18:40:09 -0800

    Use semicolon entry separator in gcc LIBRARY_PATH environment variable (#2)
    The installer script used to use a colon, which GCC docs say is the LIBRARY_PATH separator. This turns out to not be the case; LIBRARY_PATH uses semicolons as PATH does. Inserting colons caused the `LIBRARY_PATH ` environment variable to be interpreted incorrectly when appending on to an already existing `LIBRARY_PATH`.

--- packaging/install.bat
+++ packaging/install.bat
@@ -2,4 +2,4 @@ REM Adds the containing directory's "include" and "lib" subdirs to appropriate g
 
 setlocal
 setx CPLUS_INCLUDE_PATH "%~dp0include;%CPLUS_INCLUDE_PATH%"
-setx LIBRARY_PATH "%~dp0lib:%LIBRARY_PATH%"
+setx LIBRARY_PATH "%~dp0lib;%LIBRARY_PATH%"
